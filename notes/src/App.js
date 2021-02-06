bff832b83b1795a7391fa2391954ca3ad342b29e
Joshua <30259508@cityofglacol.ac.uk>
2021-02-06 23:15:03 +0000

    Refactored notes/Added show important/show all to notes

--- notes/src/App.js
+++ notes/src/App.js
@@ -1,16 +1,56 @@
+import { useState } from 'react'
 import Note from './components/Note'
 
 const App = (props) => {
-  const { notes } = props
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState('a new note')
+  const [showAll, setShowAll] = useState(true)
+
+  const addNote = (e) => {
+    e.preventDefault()
+
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random < 0.5,
+      id: notes.length + 1
+    }
+
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+  }
+
+  const handleNoteChange = (e) => {
+    console.log(e.target.value)
+    setNewNote(e.target.value)
+  }
+
+  const notesToShow = showAll
+  ? notes
+  : notes.filter(note => note.important)
 
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button 
+          className='button button-outline' 
+          onClick={() => setShowAll(!showAll)}>
+            show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
       <ul>
-        {notes.map((note) => 
+        {notesToShow.map((note) => 
           <Note key={note.id} note={note} />
         )}
       </ul>
+      <form onSubmit={addNote}>
+          <input 
+            value={newNote} 
+            onChange={handleNoteChange}
+          />
+          <button type='submit'>save</button>
+      </form>
     </div>
   )
 }
