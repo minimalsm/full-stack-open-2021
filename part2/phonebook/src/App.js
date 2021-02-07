95828ee45f5567dcae5967bfd18cfc8c365331b0
Joshua <30259508@cityofglacol.ac.uk>
2021-02-07 18:00:45 +0000

    Complete 2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,12 +2,17 @@ import { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '39-44-5323523' }
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
 
   const handleNameChange = (e) => {
     setNewName(e.target.value)
+  }
+
+  const handleNumberChange = (e) => {
+    setNewNumber(e.target.value)
   }
 
   const addPerson = (e) => {
@@ -20,8 +25,11 @@ const App = () => {
     }
 
     const personObject = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
+    console.log(personObject)
+    console.log(persons);
 
     setPersons(persons.concat(personObject))
     setNewName('')
@@ -31,23 +39,34 @@ const App = () => {
     <div className='container'>
       <h2>Phonebook</h2>
       <form onSubmit={addPerson} className='form-group'>
-        <div className='input-group'>
-          <input 
-            className='form-input' 
-            placeholder='Name'
-            value={newName}
-            onChange={handleNameChange}
-            
-          />
-          <button className='btn btn-primary input-group-btn' type="submit">Add</button>
-        </div>
+        <label className="form-label" htmlFor="name">Name</label>
+        <input 
+          className='form-input' 
+          placeholder='Name'
+          name='name'
+          id='name'
+          value={newName}
+          onChange={handleNameChange}
+        />
+
+        <label className="form-label" htmlFor="phonenumber">Phone Number</label>
+        <input 
+          className='form-input' 
+          placeholder='Phone Number'
+          name='phonenumber'
+          id='phonenumber'
+          value={newNumber}
+          onChange={handleNumberChange}
+        />
+          
+        <button className='btn btn-primary mt-2' type="submit">Add</button>
       </form>
       <h2>Numbers</h2>
       
       {persons.map((person) => 
         <div 
           key={person.name}>
-            {person.name}
+            {person.name} {person.number}
         </div>
       )}
     </div>
