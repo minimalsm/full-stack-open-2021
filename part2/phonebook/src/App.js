d8664f333a25612e128359498324aa2a10d885f4
Joshua <30259508@cityofglacol.ac.uk>
2021-02-07 18:18:22 +0000

    Complete 2.10

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,11 +1,15 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '39-44-5323523' }
-  ]) 
-  const [ newName, setNewName ] = useState('')
-  const [ newNumber, setNewNumber ] = useState('')
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
 
   const handleNameChange = (e) => {
     setNewName(e.target.value)
@@ -13,6 +17,10 @@ const App = () => {
 
   const handleNumberChange = (e) => {
     setNewNumber(e.target.value)
+  }
+
+  const handleFilterChange = (e) => {
+    setNewFilter(e.target.value)
   }
 
   const addPerson = (e) => {
@@ -35,13 +43,28 @@ const App = () => {
     setNewName('')
   }
 
+  const filteredPersons = newFilter 
+  ? persons.filter((person) => person.name.toLowerCase().includes(newFilter.toLowerCase()))
+  : persons
+
   return (
     <div className='container'>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+
+      <span>Filter shown with</span>
+      <input 
+        className='form-input col-3 col-sm-12'
+        placeholder='Filter your contacts'
+        id='filter'
+        value={newFilter}
+        onChange={handleFilterChange}
+      />
+
+      <h2>Add Contact</h2>
       <form onSubmit={addPerson} className='form-group'>
         <label className="form-label" htmlFor="name">Name</label>
         <input 
-          className='form-input' 
+          className='form-input col-3 col-sm-12' 
           placeholder='Name'
           name='name'
           id='name'
@@ -51,7 +74,7 @@ const App = () => {
 
         <label className="form-label" htmlFor="phonenumber">Phone Number</label>
         <input 
-          className='form-input' 
+          className='form-input col-3 col-sm-12' 
           placeholder='Phone Number'
           name='phonenumber'
           id='phonenumber'
@@ -59,11 +82,12 @@ const App = () => {
           onChange={handleNumberChange}
         />
           
-        <button className='btn btn-primary mt-2' type="submit">Add</button>
+        <button className='btn btn-primary mt-2 mb-2' type="submit">Add</button>
       </form>
+
       <h2>Numbers</h2>
       
-      {persons.map((person) => 
+      {filteredPersons.map((person) => 
         <div 
           key={person.name}>
             {person.name} {person.number}
