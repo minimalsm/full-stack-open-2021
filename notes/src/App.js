73636e6120f6f29461cc296174d5f49abf645880
Joshua <30259508@cityofglacol.ac.uk>
2021-02-08 21:38:25 +0000

    Complete Part 3c of Notes

--- notes/src/App.js
+++ notes/src/App.js
@@ -1,10 +1,23 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Note from './components/Note'
+import axios from 'axios'
 
 const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
-  const [newNote, setNewNote] = useState('a new note')
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+
+  useEffect(() => {
+    console.log('effect')
+
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fufilled')
+        setNotes(response.data)
+      })
+  }, [])
+  console.log('render', notes.length, 'notes')
 
   const addNote = (e) => {
     e.preventDefault()
