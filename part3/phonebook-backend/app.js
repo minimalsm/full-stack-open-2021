f0c6b2deea961560f9cbb3e163116a588a038491
Joshua <30259508@cityofglacol.ac.uk>
2021-02-10 22:05:25 +0000

    Complete 3.1

---
+++ part3/phonebook-backend/app.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+
+// app.use(express.json())
+
+let persons = [
+  {
+    id: 1,
+    name: 'Arto Hellas',
+    number: '040-123456'
+  },
+  {
+    id: 2,
+    name: 'Ada Lovelace',
+    number: '040-123456'
+  },
+  {
+    id: 3,
+    name: 'Dan Abramov',
+    number: '040-123456'
+  }
+]
+
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
