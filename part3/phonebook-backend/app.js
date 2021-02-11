f41fe991800d11f760b764c28b485452ffb5f676
Joshua <30259508@cityofglacol.ac.uk>
2021-02-11 23:02:57 +0000

    Complete 3.8

--- part3/phonebook-backend/app.js
+++ part3/phonebook-backend/app.js
@@ -3,10 +3,21 @@ const morgan = require('morgan')
 const app = express()
 
 
-
+morgan.token('post-body', function(req) {
+  return JSON.stringify(req.body)
+})
 
 app.use(express.json())
-const logger = morgan('tiny')
+const logger = morgan(function (tokens, req, res) {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    tokens['post-body'](req, res)
+  ].join(' ')
+})
 app.use(logger)
 
 let persons = [
